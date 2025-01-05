20ba47148424e9915488361d55e38a8b8b022f9c
Robert Vu <vurobert.rv@gmail.com>
2025-01-05 17:45:36 +0100

    Startade igång VS C#

---
+++ GitSkillApp/GitSkill/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GitSkill
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int secretNumb = rnd.Next(1,101);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
